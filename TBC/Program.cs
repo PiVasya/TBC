afb67e9268debc00ff802fb62e43fbe7cdbcdb56
Матвей Васильев <vasyateranit@gmail.com>
2025-02-18 01:13:58 +0300

    Первый апдейт курсового проекта

--- TBC/Program.cs
+++ TBC/Program.cs
@@ -23,7 +23,7 @@ app.MapPost("/submitt", async context =>
         Console.WriteLine($"Ошибка при создании бота: {ex.Message}");
     }
 
-    // Перенаправляем
+    // Перенаправляем что-то
     context.Response.Redirect("/index.html");
 });
 
