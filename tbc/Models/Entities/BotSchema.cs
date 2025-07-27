76608ba599dc307e4989f9dc8f5bb9f1c0bb8695
Матвей Васильев <vasyateranit@gmail.com>
2025-07-27 17:27:14 +0300

    111

--- tbc/Models/Entities/BotSchema.cs
+++ tbc/Models/Entities/BotSchema.cs
@@ -7,5 +7,6 @@ namespace tbc.Models.Entities
         public string Content { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
         public BotInstance BotInstance { get; set; } = null!;
+        public BotInstance BotInstance2 { get; set; } = null!;
     }
 }
